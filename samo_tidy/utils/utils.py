a27a3361e653655c05a0bba3dfab6c10314c7590
Sascha Moecker <Sascha.Moecker@bmw.de>
2020-12-13 17:23:10 +0100

    Fix path for Linux

--- samo_tidy/utils/utils.py
+++ samo_tidy/utils/utils.py
@@ -10,8 +10,8 @@ from clang import cindex
 def setup_clang():
     if platform.system() == "Linux":
         lib_location_file = "/usr/lib/llvm-10/lib/libclang-10.so"
-        logging.info("Searching libclang file in '%s'", lib_location)
-        cindex.Config.set_library_file("/usr/lib/llvm-10/lib/libclang-10.so")
+        logging.info("Searching libclang file in '%s'", lib_location_file)
+        cindex.Config.set_library_file(lib_location_file)
     if platform.system() == "Darwin":
         lib_location_path = "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib"
         logging.info("Searching libclang path in '%s'", lib_location_path)
