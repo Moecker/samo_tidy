6f0df3dd16ed3fbbb087991d23c6910d861e811c
Sascha Moecker <Sascha.Moecker@bmw.de>
2020-12-17 10:43:39 +0100

    Improve cindex dump

--- samo_tidy/dump/cindex_dump.py
+++ samo_tidy/dump/cindex_dump.py
@@ -3,6 +3,8 @@ from optparse import OptionParser, OptionGroup
 from pprint import pprint, pformat
 import argparse
 import logging
+import os
+import sys
 
 import samo_tidy.core.tu_parser as tu_parser
 import samo_tidy.core.compdb_parser as compdb_parser
@@ -11,52 +13,41 @@ import samo_tidy.utils.utils as utils
 import samo_tidy.dump.dump as dump
 
 
-def get_cursor_id(cursor, cursor_list=[]):
-    if cursor is None:
-        return None
-
-    # TODO: This is really slow. It would be nice if the index API exposed something that let us hash cursors.
-    for i, c in enumerate(cursor_list):
-        if cursor == c:
-            return i
-    cursor_list.append(cursor)
-    return len(cursor_list) - 1
-
-
 def get_info(node, max_depth=None, depth=0):
     if max_depth is not None and depth >= max_depth:
         children = None
     else:
-        if node.location.file and "/usr" in node.location.file.name:
+        if node.location.file and utils.shall_ignore_based_on_file_name(node.location.file.name):
             children = None
         else:
             children = [get_info(c, max_depth, depth + 1) for c in node.get_children()]
 
-    if node.location.file and "/usr" in node.location.file.name:
-        return {
-            "location (ignored)": node.location,
-        }
-    else:
-        return {
-            "kind": node.kind,
-            "usr": node.get_usr(),
-            "spelling": node.spelling,
-            "location": node.location,
-            "is_definition": node.is_definition(),
-            "->": children,
-            "tokens": ",".join([token.spelling for token in node.get_tokens()]),
-        }
+    return {
+        "kind": node.kind,
+        "usr": node.get_usr(),
+        "spelling": node.spelling,
+        "location": node.location,
+        "is_definition": node.is_definition(),
+        "->": children,
+        "tokens": ",".join([token.spelling for token in node.get_tokens()]),
+    }
 
 
 def parse_from_compdb(args):
+    the_file = None
+    the_arguments = None
+
     compdb = compdb_parser.load_compdb(args.compdb)
-    commands = compdb.getAllCompileCommands()
-    for command in commands:
-        if args.file in command.filename:
-            the_file = command.filename
-            the_arguments = list(command.arguments)
-            the_arguments = tu_parser.clean_args(the_arguments)
-            the_arguments = tu_parser.absolute_path_include(the_arguments, command.directory)
+    if compdb:
+        commands = compdb.getAllCompileCommands()
+        for command in commands:
+            if args.file in command.filename:
+                the_file = os.path.join(command.directory, command.filename)
+                the_arguments = list(command.arguments)
+                the_arguments = tu_parser.clean_args(the_arguments)
+                the_arguments = tu_parser.absolute_path_include(the_arguments, command.directory)
+    else:
+        sys.exit("Failed to load compdb")
     return the_file, the_arguments
 
 
@@ -95,8 +86,11 @@ def main():
     # Use compdb infos if available
     if args.compdb:
         the_file, the_arguments = parse_from_compdb(args)
+        if the_file == None:
+            sys.exit(f"File {args.file} not found in compdb")
 
     # Only parse the file provided by the args
+    logging.info("Parsing %s with %s", the_file, the_arguments)
     tu = index.parse(the_file, the_arguments)
     if not tu:
         logging.error("Unable to load input")
