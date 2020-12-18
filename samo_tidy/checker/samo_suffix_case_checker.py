12f2b4b2cd8057ba956ed8eb405f406b027c3b31
Sascha Moecker <Sascha.Moecker@bmw.de>
2020-12-18 10:52:12 +0100

    Naive implementation of fixit

--- samo_tidy/checker/samo_suffix_case_checker.py
+++ samo_tidy/checker/samo_suffix_case_checker.py
@@ -13,3 +13,18 @@ def token_based_rule(token):
                         child_token, "TIDY_SAMO_SUFFIX_CASE", "Lower Case suffix for unsigned integer"
                     )
     return violation
+
+
+def fixit(violation):
+    lines = []
+    with open(violation.file_path) as the_file:
+        lines = the_file.readlines()
+
+    violated_line = list(lines[violation.line])
+    violated_line[violation.column] = violated_line[violation.column].upper()
+    fixed_line = "".join(violated_line)
+
+    lines[violation.line] = fixed_line
+
+    with open(violation.file_path, "w") as the_file:
+        the_file.writelines(lines)
--- samo_tidy/fixit/test/test_fixit.py
+++ samo_tidy/fixit/test/test_fixit.py
@@ -2,11 +2,21 @@ import unittest
 
 import samo_tidy.fixit.fixit as fixit
 import samo_tidy.test.test_support as test_support
+import samo_tidy.checker.samo_suffix_case_checker as samo_suffix_case_checker
+from samo_tidy.checker.violation import Violation
 
 
 class TestFacade(unittest.TestCase):
-    def test_missing_arguments(self):
+    def test_dummy(self):
         self.assertTrue(True)
+
+    def test_fixit(self):
+        filename = test_support.create_tempfile(["std::uint8_t var = 1u;"])
+        violation = Violation("TIDY_SAMO_SUFFIX_CASE", "", filename, 0, 20)
+
+        samo_suffix_case_checker.fixit(violation)
+        with open(violation.file_path) as the_file:
+            self.assertEqual(the_file.readlines()[violation.line].strip(), "std::uint8_t var = 1U;")
 
 
 if __name__ == "__main__":
